b2d296b3727f31bf17a6cb48211bd07610fa4877
0x00-pl <0x00.pl@gmail.com>
2025-04-14 17:33:45 +0800

    use Path for model_path

--- backend/predictor.py
+++ backend/predictor.py
@@ -8,8 +8,8 @@ class EmotionClassifier:
     def __init__(self, model_path="./emotion_model_12emo"):
         """加载12类情绪分类模型"""
         # 加载模型和分词器
-        model_path = Path("./backend/emotion_model_12emo").resolve()
-        self.tokenizer = BertTokenizer.from_pretrained(str(model_path), local_files_only=True)
+        model_path = Path(model_path).resolve()
+        self.tokenizer = BertTokenizer.from_pretrained(model_path, local_files_only=True)
         self.model = BertForSequenceClassification.from_pretrained(model_path)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
